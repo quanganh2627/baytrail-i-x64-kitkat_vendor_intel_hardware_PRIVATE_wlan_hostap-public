4632ef6ff1bb200e38cfad7461d191a78f5c230b
Ilan Peer <ilan.peer@intel.com>
2013-12-11 14:17:05 +0200

    nl80211: clear beacon_set when deleting a beacon
    When a beacon was deleted from the kernel in wpa_driver_nl80211_deinit_ap(),
    bss->beacon_set was not cleared so it restarting the ap again was not
    possible.
    
    Fix this by clearing the variable once the beacon was deleted.
    
    Change-Id: Ie75019f591cea673fc712918bd6408ebf52a984b
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/27565
    Tested-by: IWL Jenkins
    Reviewed-by: David Spinadel <david.spinadel@intel.com>
    
    x-iwlwifi-hostap-dev-build: 64af35276f017532403aef28a95156ca70b07782

--- src/common/version.h
+++ src/common/version.h
@@ -6,5 +6,5 @@
 #endif /* VERSION_STR_POSTFIX */
 
 
-#define VERSION_STR "2.0-WCD-hostap-public-bc40e1d21af2ed61f2808610fbd45bbbf2bc6fdb" VERSION_STR_POSTFIX
+#define VERSION_STR "2.0-WCD-hostap-public-64af35276f017532403aef28a95156ca70b07782" VERSION_STR_POSTFIX
 #endif /* VERSION_H */
--- src/drivers/driver_nl80211.c
+++ src/drivers/driver_nl80211.c
@@ -10060,6 +10060,7 @@ static int wpa_driver_nl80211_deinit_ap(void *priv)
 	if (!is_ap_interface(drv->nlmode))
 		return -1;
 	wpa_driver_nl80211_del_beacon(drv);
+	bss->beacon_set = 0;
 
 	/*
 	 * If the P2P GO interface was dynamically added, then it is
