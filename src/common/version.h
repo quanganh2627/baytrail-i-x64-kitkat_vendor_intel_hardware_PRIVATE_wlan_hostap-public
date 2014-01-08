121451bf0d75ac9f8aec8f4f4e2eb8d365ecc04e
Beni Lev <beni.lev@intel.com>
2014-01-08 15:54:56 +0300

    wpa_cli: Add vendor command support
    Vendor command's format:
    VENDOR <vendor id> <command id>[ <null terminated hex formatted data>]
    
    The 3rd argument will be converted to binary and then passed as argument
    to the sub command.
    
    Change-Id: Iaa3bcf7ebcdb430b545b215989a68c3423aed34b
    Signed-off-by: Beni Lev <beni.lev@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/28755
    Reviewed-by: Ilan Peer <ilan.peer@intel.com>
    Tested-by: Ilan Peer <ilan.peer@intel.com>
    
    x-iwlwifi-hostap-dev-build: 74daa7d5b9e2258ccc02a61ab7ef9fe3b2e88817

--- src/common/version.h
+++ src/common/version.h
@@ -5,5 +5,5 @@
 #define VERSION_STR_POSTFIX ""
 #endif /* VERSION_STR_POSTFIX */
 
-#define VERSION_STR "2.0-WCD-hostap-public-19161c538eeb8328800d0285ec0fd17c859f8e17" VERSION_STR_POSTFIX
+#define VERSION_STR "2.0-WCD-hostap-public-74daa7d5b9e2258ccc02a61ab7ef9fe3b2e88817" VERSION_STR_POSTFIX
 #endif /* VERSION_H */
