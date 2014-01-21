891d76c2a17d77e9bd692dbbf80735917305f02e
Max Stepanov <Max.Stepanov@intel.com>
2014-01-21 18:40:13 +0200

    wpa_supplicant: fix of seg fault in wpas_ctrl_radio_work_flush
    Added verification of wpa_s->radio pointer.
    
    The segmentation fault details:
    
    Program received signal SIGSEGV, Segmentation fault.
    0x00000000004b9591 in wpas_ctrl_radio_work_flush (wpa_s=0x77fff0) at ctrl_iface.c:5754
    5754		dl_list_for_each_safe(work, tmp, &wpa_s->radio->work,
    
    Call stack:
    0  wpas_ctrl_radio_work_flush (wpa_s=0x77fff0) at ctrl_iface.c:5754
    1  wpa_supplicant_deinit_iface (wpa_s=0x77fff0, notify=0, terminate=0) at wpa_supplicant.c:3619
    2  wpa_supplicant_add_iface (global=0x75db10, iface=0x7fffffffe270) at wpa_supplicant.c:3691
    3  wpas_p2p_add_p2pdev_interface (wpa_s=0x75dd20) at p2p_supplicant.c:3700
    4  main (argc=<optimized out>, argv=<optimized out>) at main.c:317
    
    Function:
    5750	void wpas_ctrl_radio_work_flush(struct wpa_supplicant *wpa_s)
    5751	{
    5752		struct wpa_radio_work *work, *tmp;
    5753
    5754		dl_list_for_each_safe(work, tmp, &wpa_s->radio->work,
    5755				      struct wpa_radio_work, list) {
    5756			struct wpa_external_work *ework;
    5757
    5758			if (os_strncmp(work->type, "ext:", 4) != 0)
    
    Root cause:
    (gdb) p wpa_s->radio
    $1 = (struct wpa_radio *) 0x0
    
    Change-Id: If6169c2d6e31ee3d8d3b890e69253fcba8e2acfb
    Signed-off-by: Max Stepanov <Max.Stepanov@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/29388
    Tested-by: IWL Jenkins
    Reviewed-by: Ilan Peer <ilan.peer@intel.com>
    
    x-iwlwifi-hostap-dev-build: d195f4a50dd27d18e354c39abedd675fdc6b84fc

--- src/common/version.h
+++ src/common/version.h
@@ -6,5 +6,5 @@
 #endif /* VERSION_STR_POSTFIX */
 
 
-#define VERSION_STR "2.0-WCD-hostap-public-2c90c0adfe160f331122648e0c0aacb0a051ecbb" VERSION_STR_POSTFIX
+#define VERSION_STR "2.0-WCD-hostap-public-d195f4a50dd27d18e354c39abedd675fdc6b84fc" VERSION_STR_POSTFIX
 #endif /* VERSION_H */
