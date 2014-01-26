3223806d9f63670c2c3c46ac8ad16058d625f1c0
Beni Lev <beni.lev@intel.com>
2014-01-26 18:16:35 +0300

    nl80211: Add vendor command support.
    nl80211 implementation of vendor command.
    
    Change-Id: I5bda5656c03c898a4864deed29d913572b569773
    Signed-off-by: Beni Lev <beni.lev@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/28753
    Reviewed-by: Max Stepanov <Max.Stepanov@intel.com>
    Tested-by: IWL Jenkins
    Reviewed-by: Ilan Peer <ilan.peer@intel.com>
    
    x-iwlwifi-hostap-dev-build: cba7870ec4fb945724db328acd90bb9a12123a07

--- src/common/version.h
+++ src/common/version.h
@@ -5,5 +5,5 @@
 #define VERSION_STR_POSTFIX ""
 #endif /* VERSION_STR_POSTFIX */
 
-#define VERSION_STR "2.0-WCD-hostap-public-fab4acad12965e69f8dfe99baef1ca27b9b68e79" VERSION_STR_POSTFIX
+#define VERSION_STR "2.0-WCD-hostap-public-cba7870ec4fb945724db328acd90bb9a12123a07" VERSION_STR_POSTFIX
 #endif /* VERSION_H */
