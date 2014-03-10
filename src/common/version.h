fca656e14ffd0d15b718f88d0d01ba8c1e771d70
Ilan Peer <ilan.peer@intel.com>
2014-03-10 13:43:04 +0200

    [WCD] wpa_supplicant: enable EAP_PWD in all configurations
    Change-Id: I9adae89dd4f57213580126b22c197907d5def928
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/32560
    
    x-iwlwifi-hostap-dev-build: b08e1a4e1e7e6ca24b4304ba01d4b1086ee57944

--- src/common/version.h
+++ src/common/version.h
@@ -5,5 +5,5 @@
 #define VERSION_STR_POSTFIX ""
 #endif /* VERSION_STR_POSTFIX */
 
-#define VERSION_STR "2.0-WCD-hostap-public-13d2fc76a330ccff62bdb4b0c9a54f19e7fc26b0" VERSION_STR_POSTFIX
+#define VERSION_STR "2.0-WCD-hostap-public-b08e1a4e1e7e6ca24b4304ba01d4b1086ee57944" VERSION_STR_POSTFIX
 #endif /* VERSION_H */
