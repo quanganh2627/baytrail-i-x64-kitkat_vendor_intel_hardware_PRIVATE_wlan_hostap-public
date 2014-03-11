37fa43555e58d21ced3106f089aa8ed67a91fd59
Ilan Peer <ilan.peer@intel.com>
2014-03-11 11:46:22 +0200

    [WCD] P2P: change the max client inactivity timeout to 1 min.
    Change-Id: I66a4b4a0e0c5d93511e90ee75c5b17934162e946
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/32708
    Reviewed-by: David Spinadel <david.spinadel@intel.com>
    Reviewed-by: Oz Shalev <oz.shalev@intel.com>
    Tested-by: IWL Jenkins
    
    x-iwlwifi-hostap-dev-build: 32684c155e00bcf3209ce43f2eb62e599ddf9636

--- src/common/version.h
+++ src/common/version.h
@@ -5,5 +5,5 @@
 #define VERSION_STR_POSTFIX ""
 #endif /* VERSION_STR_POSTFIX */
 
-#define VERSION_STR "2.0-WCD-hostap-public-6bdaa270937fa4660f6bb147ab04b67d8daa9cda" VERSION_STR_POSTFIX
+#define VERSION_STR "2.0-WCD-hostap-public-32684c155e00bcf3209ce43f2eb62e599ddf9636" VERSION_STR_POSTFIX
 #endif /* VERSION_H */
