31f0060efd1d8ae70ead351c117ba9ed724fb67e
Ilan Peer <ilan.peer@intel.com>
2014-03-23 10:03:55 +0200

    wpa_supplicant: remove pending sme-connect radio work [BUGFIX]
    If a new connection is attempted while there is a pending
    sme-connection radio work, cancel the pending radio work
    and continue with the new connection attempt.
    
    This is preferable over rejecting the new working and
    continuing with the pending one, as it is possible that the
    previous work is no longer valid.
    
    ---
    
    MWG100201156
    
    Change-Id: I3979b2695fe47e1f1b643ff6231230301bba2fd0
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/33544
    Tested-by: IWL Jenkins
    Reviewed-by: Andrei Otcheretianski <andrei.otcheretianski@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/33565
    
    x-iwlwifi-hostap-dev-build: 94e142bdbf8495c7880887c1faf428ecdb213680

--- src/common/version.h
+++ src/common/version.h
@@ -5,5 +5,5 @@
 #define VERSION_STR_POSTFIX ""
 #endif /* VERSION_STR_POSTFIX */
 
-#define VERSION_STR "2.0-WCD-hostap-public-bb78cc8936479e71cf1d5c39c9772fcf16d9e1ff" VERSION_STR_POSTFIX
+#define VERSION_STR "2.0-WCD-hostap-public-94e142bdbf8495c7880887c1faf428ecdb213680" VERSION_STR_POSTFIX
 #endif /* VERSION_H */
