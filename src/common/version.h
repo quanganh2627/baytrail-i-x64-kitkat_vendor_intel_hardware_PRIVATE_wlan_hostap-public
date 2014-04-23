e5c9c83d5bc1a9faa5af900923782ee48c44b5d9
Ilan Peer <ilan.peer@intel.com>
2014-04-23 10:02:34 +0300

    wpa_supplicant: call frequency conflict handling during auth
    Currently, the frequency conflict is handled only during the
    association flow. However, some drivers, e.g., mac80211 based
    drivers, will fail an authentication request in case that there
    are no available channels for use (as they might be used by
    other interfaces), and thus the frequency conflict resolution
    is never called.
    
    Fix this by calling frequency conflict resolution during
    authentication.
    
    In addition get the shared radio frequency from the wpa_s
    context and not from the driver, as the driver might not
    consider all interface types.
    
    Change-Id: I38724cce472f0eff7c8547e6f4ce952cae0e2b48
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/35625
    Reviewed-by: Max Stepanov <Max.Stepanov@intel.com>
    Tested-by: IWL Jenkins
    Reviewed-on: https://gerrit.rds.intel.com/35782
    Reviewed-by: Assaf Azulay <assaf.azulay@intel.com>
    Reviewed-by: Guy Cohen <guy.cohen@intel.com>
    
    x-iwlwifi-hostap-dev-build: 815c1aba74f01c46703c4f5ccdaf00e6996a3c84

--- src/common/version.h
+++ src/common/version.h
@@ -5,5 +5,5 @@
 #define VERSION_STR_POSTFIX ""
 #endif /* VERSION_STR_POSTFIX */
 
-#define VERSION_STR "2.0-WCD-hostap-public-3deebb2dbab2c9e62f1372c760b5f19a4f4d8298" VERSION_STR_POSTFIX
+#define VERSION_STR "2.0-WCD-hostap-public-815c1aba74f01c46703c4f5ccdaf00e6996a3c84" VERSION_STR_POSTFIX
 #endif /* VERSION_H */
