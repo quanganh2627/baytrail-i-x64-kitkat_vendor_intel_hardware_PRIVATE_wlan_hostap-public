8cd57b21b6f424ae8f126d099e81e0d24f86022b
Jouni Malinen <jouni@qca.qualcomm.com>
2014-04-25 23:19:47 +0300

    Make last_scan_res update easier for static analyzers
    BZ: 505
    
    The check based on last_scan_res_used is sufficient for making sure that
    last_scan_res is allocated. However, it is a bit too complex for static
    analyzers to notice, so add an explicit check to avoid bogus reports.
    
    Change-Id: I8a5c415148046572371955f63985cefb88c6da5d
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>
    Reviewed-on: https://gerrit.rds.intel.com/r/49143
    Reviewed-by: Guy Cohen <guy.cohen@intel.com>
    Reviewed-by: Ilan Peer <ilan.peer@intel.com>
    Tested-by: IWL Jenkins
    
    x-iwlwifi-hostap-dev-build: 0c1c3f85ee2434e36cc544bde2849d9a4734dca1

--- src/common/version.h
+++ src/common/version.h
@@ -13,5 +13,5 @@
 #define VERSION_STR_POSTFIX ""
 #endif /* VERSION_STR_POSTFIX */
 
-#define VERSION_STR "hostap-public:release/LinuxCore5:1:eea2fa23" VERSION_STR_POSTFIX
+#define VERSION_STR "hostap-public:release/LinuxCore5:1:0c1c3f85" VERSION_STR_POSTFIX
 #endif /* VERSION_H */
